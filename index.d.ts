ec59b04e7917959da5999b283ce5064c53c19eb4
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2019-10-07 16:42:54 +0200

    Add TS type definitions

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+// Definitions by: ricardo-devis-agullo <https://github.com/ricardo-devis-agullo>
+
+import fastify = require('fastify');
+
+import { Server, IncomingMessage, ServerResponse } from 'http';
+
+declare module 'fastify' {
+  interface FastifyInstance<HttpServer, HttpRequest, HttpResponse> {
+    basicAuth: FastifyMiddleware<HttpServer, HttpRequest, HttpResponse>;
+  }
+}
+
+declare const fastifyBasicAuth: fastify.Plugin<
+  Server,
+  IncomingMessage,
+  ServerResponse,
+  {
+    validate: (
+      username: string,
+      password: string,
+      req: fastify.FastifyRequest,
+      reply: fastify.FastifyReply<ServerResponse>,
+      done?: (err?: Error) => void
+    ) => void;
+    authenticate?: boolean | { realm: string };
+  }
+>;
+
+export = fastifyBasicAuth;
