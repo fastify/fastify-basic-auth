0da69d72580a9072a6e8ef1777742727151cebb4
Philipp A <flying-sheep@web.de>
2021-09-15 16:49:36 +0200

    Fix validate() return type

--- index.d.ts
+++ index.d.ts
@@ -23,7 +23,7 @@ export interface FastifyBasicAuthOptions {
     req: FastifyRequest,
     reply: FastifyReply,
     done: (err?: Error) => void
-  ): void | Promise<void>;
+  ): Error | void | Promise<Error | void>;
   authenticate?: boolean | { realm: string };
   header?: string;
 }
