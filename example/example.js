cb840c87b9bed9b7dcae273fe7da9d48528137ea
uzlopak <aras.abbasi@googlemail.com>
2022-11-29 15:51:22 +0100

    use timingSafeEqual

--- example/example.js
+++ example/example.js
@@ -1,15 +1,33 @@
 'use strict'
 
 const fastify = require('fastify')()
-const authenticate = {realm: 'Westeros'}
+const crypto = require('crypto')
+const authenticate = { realm: 'Westeros' }
 
 const validUsername = 'Tyrion'
 const validPassword = 'wine'
 
 fastify.register(require('..'), { validate, authenticate })
+
+// perform constant-time comparison to prevent timing attacks
+function compare (a, b) {
+  a = Buffer.from(a)
+  b = Buffer.from(b)
+  if (a.length !== b.length) {
+    // Delay return with cryptographically secure timing check.
+    crypto.timingSafeEqual(a, a)
+    return false
+  }
+
+  return crypto.timingSafeEqual(a, b)
+}
+
 // `this` inside validate is `fastify`
 function validate (username, password, req, reply, done) {
-  if (username === validUsername && password === validPassword) {
+  let result = true
+  result = compare(username, validUsername) && result
+  result = compare(password, validPassword) && result
+  if (result) {
     done()
   } else {
     done(new Error('Winter is coming'))
